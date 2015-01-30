e8bef196194934f5192ca2d9f2acc9768dc0c418
elizabrock <eliza@elizabrocksoftware.com>
2015-01-30 10:09:55 -0600

    Pull in code from Wednesday's live coding, via @AlexStory and @JaclynPasquale.

--- GrapeShapes/MainWindow.xaml.cs
+++ GrapeShapes/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace GrapeShapes
         {
             InitializeComponent();
             PopulateClassList();
+            PopulateTestShapes();
         }
 
         public static int ArgumentCountFor(string className)
@@ -53,6 +54,74 @@ namespace GrapeShapes
             }
             ShapeType.ItemsSource = classList;
         }
+
+        private void PopulateTestShapes()
+        {
+            var square = new Square(30);
+            square.FillColor = System.Drawing.Color.AliceBlue;
+            square.BorderColor = System.Drawing.Color.BurlyWood;
+
+            var square2 = new Square(200);
+            square2.BorderColor = System.Drawing.Color.Navy;
+            square2.FillColor = System.Drawing.Color.Fuchsia;
+            DrawSquare(1, 50, square);
+            DrawSquare(50, 5, square2);
+        }
+
+
+        private void DrawSquare(int x, int y, Square size)
+        {
+            System.Windows.Shapes.Polygon myPolygon = new System.Windows.Shapes.Polygon();
+
+            SolidColorBrush border = new SolidColorBrush();
+            border.Color = Color.FromArgb(size.BorderColor.A, size.BorderColor.R, size.BorderColor.G, size.BorderColor.B);
+
+            SolidColorBrush fill = new SolidColorBrush();
+            fill.Color = Color.FromArgb(size.FillColor.A, size.FillColor.R, size.FillColor.G, size.FillColor.B);
+
+            myPolygon.Stroke = border;
+            myPolygon.Fill = fill;
+            myPolygon.StrokeThickness = 2;
+            myPolygon.HorizontalAlignment = HorizontalAlignment.Left;
+            myPolygon.VerticalAlignment = VerticalAlignment.Center;
+            Point point1 = new Point(x, y);
+            Point point2 = new Point(x, y + (double)size.Width);
+            Point point3 = new Point(x + (double)size.Width, y + (double)size.Width);
+            Point point4 = new Point(x + (double)size.Width, y);
+
+            PointCollection myPointCollection = new PointCollection();
+            myPointCollection.Add(point1);
+            myPointCollection.Add(point2);
+            myPointCollection.Add(point3);
+            myPointCollection.Add(point4);
+
+            myPolygon.Points = myPointCollection;
+            ShapeCanvas.Children.Add(myPolygon);
+        }
+
+        private void DrawRectangle()
+        {
+            System.Windows.Shapes.Polygon myPolygon = new System.Windows.Shapes.Polygon();
+            myPolygon.Stroke = System.Windows.Media.Brushes.Tomato;
+            myPolygon.Fill = System.Windows.Media.Brushes.Bisque;
+            myPolygon.StrokeThickness = 2;
+            myPolygon.HorizontalAlignment = HorizontalAlignment.Left;
+            myPolygon.VerticalAlignment = VerticalAlignment.Center;
+            Point point1 = new Point(10, 50);
+            Point point2 = new Point(10, 80);
+            Point point3 = new Point(50, 80);
+            Point point4 = new Point(50, 50);
+
+            PointCollection myPointCollection = new PointCollection();
+            myPointCollection.Add(point1);
+            myPointCollection.Add(point2);
+            myPointCollection.Add(point3);
+            myPointCollection.Add(point4);
+
+            myPolygon.Points = myPointCollection;
+            ShapeCanvas.Children.Add(myPolygon);
+        }
+
 
         private void ShapeType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
